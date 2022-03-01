849f329fdacbd2b36c586e13f9027c34c4e5462a
yeni <halukbilgin.uk@gmail.com>
2022-03-01 21:19:08 +0300

    .

--- src/_03_IfStatement_TernaryOperator/Q01.java
+++ src/_03_IfStatement_TernaryOperator/Q01.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class Q01 {
 
-	public static void main(String[] args) {
-		/* TASK :
-		 * Kullanicidan alacaginiz iki sayiyi yine kullaniciya sectireceginiz
-		 *  dort islemden biri ile isleme koyup sonucun yazdiriniz
-		 */
-		
+    public static void main(String[] args) {
+        /* TASK :
+         * Kullanicidan alacaginiz iki sayiyi yine kullaniciya sectireceginiz
+         *  dort islemden biri ile isleme koyup sonucun yazdiriniz
+         */
 
-		
-	}
+        Scanner scan = new Scanner(System.in);//kullanıcıdan input almak için canner obj create ettik.
+        System.out.println(" toplama için 1\n çikarma için 2\n bolme için 3\n çarpma için 4");
+        //kullanıcıya seçim içn işlem menusu yazdırdık
+        int islem = scan.nextInt();//kullanıcının islem tercihi için veriable create edildi
+        System.out.println("lütfen iki tam sayı giriniz : ");//kullanıcıya bildirmde bulunuldu.
 
+        double num1 = scan.nextDouble();//1 sayı assign edildi
+        double num2 = scan.nextDouble();//2 sayı assign edildi
+        if (islem == 1) {
+            System.out.println("toplama işleminin sonucu :" + num1 + " + " + num2 + " = " + (num1 + num2));
+        }else if (islem == 2) {
+            System.out.println("cıkarma işleminin sonucu :" + num1 + " - " + num2 + " = " + (num1 - num2));
+        } else if (islem == 3) {
+            System.out.println("bolme işleminin sonucu :" + num1 + " / " + num2 + " = " + (num1 / num2));
+        } else if (islem == 4) {
+            System.out.println("carpma işleminin sonucu :" + num1 + " x " + num2 + " = " + (num1 * num2));
+        } else System.out.println("hatalı secim yaptınız tekrar deneyiniz");
+
+    }
 }
--- src/_03_IfStatement_TernaryOperator/Q03.java
+++ src/_03_IfStatement_TernaryOperator/Q03.java
@@ -6,10 +6,10 @@ public class Q03 {
 
 	public static void main(String[] args) {
 		/* TASK :
-		 *  Kullanicinin boyunu  CM  ve kilosunu kg alarak  BMI (VKE) hesaplayiniz
+		 *  Kullanicinin boyunu  m  ve kilosunu kg alarak  BMI (VKE) hesaplayiniz
 
 	    INFO :
-		BMI = kilo(kg) /(boy*boy)(cm)
+		BMI = kilo(kg) /(boy*boy)(m)
 		BMI <=20 oldukca zayifsiniz
 		20<BMI<=25 Normal sinirlardasiniz 
 		25<BMI<=30 Sisman kategorisindesiniz
@@ -18,11 +18,24 @@ public class Q03 {
 		 */
 
 
+        Scanner scan = new Scanner(System.in);//kullanıcıdan input almak için canner obj create ettik.
 
+        System.out.println("boyunuzu cm giriniz : ");
+        double boy=scan.nextDouble();
 
+        System.out.println("kilonuzu  kg giriniz : ");
+        double kilo=scan.nextDouble();
 
-
-
+        double bmi=kilo/((boy/100)*(boy/100));
+        if (bmi<=20){
+            System.out.println("oldukca zayıfsınız");
+        } else if (bmi>20 & bmi<=25){
+            System.out.println("normal sınırlardasınz");
+        } else if (bmi>25 & bmi<=30){
+            System.out.println("Sisman kategorisindesiniz");
+        } else if (bmi>30){
+            System.out.println("Obez grubundasiniz.");
+        }
     }
 
 }
