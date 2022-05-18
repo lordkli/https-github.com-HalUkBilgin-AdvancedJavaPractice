6d11499667d513131ed9cb8e8a97fe83ab8ae22f
yeni <halukbilgin.uk@gmail.com>
2022-05-18 09:49:11 +0300

    the end

--- src/JavaProjects/Projects_01/Main.java
+++
@@ -1,8 +0,0 @@
-package JavaProjects.Projects_01;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Goodbye");
-    }
-}
--- src/JavaProjects/Projects_02/Main2.java
+++
@@ -1,7 +0,0 @@
-package JavaProjects.Projects_02;
-
-public class Main2 {
-    public static void main(String[] args) {
-        System.out.println(2);
-    }
-}
